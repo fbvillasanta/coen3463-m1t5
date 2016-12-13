2e005aa3a6d762023020643d957b255cad8232d5
fbvillasanta <fbvillasanta@yahoo.com>
2016-12-13 13:11:29 +0800

    re commit

---
+++ express.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.get('/', function(req, res){
+	res.send('HELLO WORLD!');
+});
+
+app.get('/home/:name?', function(req, res){
+	var name = req.params.name;
+	res.send('HOME of ' + name );
+});
+
+app.get('/about', function(req, res){
+	//res.sendFile('about.html', {"root":__dirname});
+	var absPath = path.join(__dirname, 'about.html');
+	console.log(absPath);
+	res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/services', function(req, res){
+	res.sendFile(path.join(__dirname, 'services.html'));
+});*/
+
+app.listen('3000', function(){
+	console.log('Express is runnning on port 3000');
+});
