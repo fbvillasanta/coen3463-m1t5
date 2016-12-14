975dfd884639a142967a98dadebe1a17a752c6e2
fbvillasanta <fbvillasanta@yahoo.com>
2016-12-14 16:24:10 +0800

    html files

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
- var express = require("express");
-    var app = express();
+/* var express = require("express");
+ var app = express();
 
-    app.get('/', function(req, res) {
-      res.send('Hello World!');
-    });
+ app.get('/', function(req, res) {
+    res.send('Hello World!');
+  });
 
-    var port = Number(process.env.PORT || 5000);
-    app.listen(port, function() {
-      console.log("Listening on " + port);
-    });
+ var port = Number(process.env.PORT || 5000);
+ app.listen(port, function() {
+    console.log("Listening on " + port);
+ });*/
 
-/*var express = require('express');
+var express = require('express');
 var path = require('path');
 var app = express();
 
@@ -34,8 +34,8 @@ app.get('/about', function(req, res){
 
 app.get('/services', function(req, res){
 	res.sendFile(path.join(__dirname, 'services.html'));
-}); -- comment out
+});*/
 
 app.listen('3000', function(){
 	console.log('Express is runnning on port 3000');
-});*/
+});
