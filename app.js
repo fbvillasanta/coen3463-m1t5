106c4f37b839ce3bd7cea26fd42c916e779c2071
fbvillasanta <fbvillasanta@yahoo.com>
2016-12-14 15:44:49 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-const http = require('http');
+ var express = require("express");
+    var app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+    app.get('/', function(req, res) {
+      res.send('Hello World!');
+    });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    var port = Number(process.env.PORT || 5000);
+    app.listen(port, function() {
+      console.log("Listening on " + port);
+    });
+
+/*var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.get('/', function(req, res){
+	res.send('HELLO WORLD!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/home/:name?', function(req, res){
+	var name = req.params.name;
+	res.send('HOME of ' + name );
 });
+
+app.get('/about', function(req, res){
+	//res.sendFile('about.html', {"root":__dirname});
+	var absPath = path.join(__dirname, 'about.html');
+	console.log(absPath);
+	res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/services', function(req, res){
+	res.sendFile(path.join(__dirname, 'services.html'));
+}); -- comment out
+
+app.listen('3000', function(){
+	console.log('Express is runnning on port 3000');
+});*/
